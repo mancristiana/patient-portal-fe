a7b44b167def82d07fb59f8e4cdbb6435b8f0b49
Cristiana Man <man.cristiana1@gmail.com>
2018-05-11 17:41:38 +0200

    Created Hello component and addeded test setup

---
+++ src/components/Hello.test.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import * as Enzyme from 'enzyme';
+
+import Hello from './Hello';
+
+it('renders the correct text when no enthusiasm level is given', () => {
+  const hello = Enzyme.shallow(<Hello name="Cristiana" />);
+  expect(hello.find('.greeting').text()).toEqual('Hello Cristiana!');
+});
+
+it('renders the correct text with an explicit enthusiasm of 1', () => {
+  const hello = Enzyme.shallow(<Hello name="Cristiana" enthusiasmLevel={1} />);
+  expect(hello.find('.greeting').text()).toEqual('Hello Cristiana!');
+});
+
+it('renders the correct text with an explicit enthusiasm level of 5', () => {
+  const hello = Enzyme.shallow(<Hello name="Cristiana" enthusiasmLevel={5} />);
+  expect(hello.find('.greeting').text()).toEqual('Hello Cristiana!!!!!');
+});
+
+it('throws when the enthusiasm level is 0', () => {
+  expect(() => {
+    Enzyme.shallow(<Hello name="Cristiana" enthusiasmLevel={0} />);
+  }).toThrow();
+});
+
+it('throws when the enthusiasm level is negative', () => {
+  expect(() => {
+    Enzyme.shallow(<Hello name="Cristiana" enthusiasmLevel={-1} />);
+  }).toThrow();
+});
