a8d175b3d72328e5c4dc9909012439425c40c6e6
Cristiana Man <man.cristiana1@gmail.com>
2018-05-19 17:27:29 +0200

    Add home view

---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import './Home.less';
+
+class Home extends React.PureComponent {
+  public render() {
+    return <div className="Home">Hello Home!</div>;
+  }
+}
+
+export default Home;
