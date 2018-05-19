7db4feda821ac30d2e7175cceeaa4cbb5f15b037
Cristiana Man <man.cristiana1@gmail.com>
2018-05-19 13:50:32 +0200

    Added brand customizations to antd

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,30 @@
+const { getLoader } = require('react-app-rewired');
+const tsImportPluginFactory = require('ts-import-plugin');
+
 module.exports = function override(config, env) {
+  // get tsloader
+  const tsloader = getLoader(
+    config.module.rules,
+    rule => String(rule.test) == String(/\.(ts|tsx)$/)
+  );
+
+  // set new options
+  tsloader.options = {
+    transpileOnly: true,
+    getCustomTransformers: () => ({
+      before: [
+        tsImportPluginFactory([
+          {
+            libraryName: 'antd',
+            libraryDirectory: 'lib'
+          },
+          {
+            libraryName: 'antd-mobile',
+            libraryDirectory: 'lib'
+          }
+        ])
+      ]
+    })
+  };
   return config;
 };
